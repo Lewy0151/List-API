0b6acc98b381be18b4c12e66cabe089a3ea34bfc
Lewy0151 <lewiscargill@hotmail.com>
2025-05-21 10:20:54 +0100

    Deployment

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -83,7 +83,7 @@ exports.getMovieByTitle = async (request, response) => {
 exports.getMovieByDirector = async (request, response) => {
   try {
     const movies = await Movie.find({
-      director: req.params.director,
+      director: req.params.director
     });
     response.send(movies);
   } catch (error) {
